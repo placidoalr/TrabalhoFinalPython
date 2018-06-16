9d20c609a0bc45786ed4f1d0c4672bd3f5e1b625
placidoalr <placido.inter@gmail.com>
2018-06-16 13:01:19 -0300

    Quase pronto, só falta imprimir os dados

--- AtividadeEnem.py
+++ AtividadeEnem.py
@@ -1,25 +1,13 @@
-#Utilizar o pandas
-# Utilizar nu_inscricao para pegar o numero de inscrição
-# Utilizar nu_idade para pegar a idade 
-# Utilizar nu_nota_cn nota de ciencias da natureza
-# Utilizar nu_nota_ch nota de ciencias humanas
-# Utilizar nu_nota_lc nota de linguagens e codigos
-# Utilizar nu_nota_mt nota de matematica
-# Utilizar nu_nota_redacao nota da redação
-# precisa fazer o calculo ((nu_nota_mt x 3)+(nu_nota_cn x 2)+(nu_nota_lc x 1.5)+(nu_nota_ch)+(nu_nota_redacao x 3))/5 
-#DROP NaN para derrubar quem não foi nas provas
-#FALTA SORT
-
-
 import pandas as pd
-import csv
-dataset = pd.read_csv('C:\\GitHub\\TrabalhoFinalPython\\train.csv', sep=',')
-#print(dataset.head())
-#print(dataset['NU_IDADE'])
-
-data = dataset.dropna(subset=['NU_NOTA_REDACAO','NU_NOTA_MT','NU_NOTA_LC','NU_NOTA_CH','NU_NOTA_CN'])
+data = pd.read_csv('C:\\GitHub\\TrabalhoFinalPython\\train.csv', sep=',')
+data = data.loc[:,['NU_INSCRICAO','NU_NOTA_REDACAO','NU_NOTA_MT','NU_NOTA_LC','NU_NOTA_CH','NU_NOTA_CN']]
+data = data.dropna(subset=['NU_NOTA_REDACAO','NU_NOTA_MT','NU_NOTA_LC','NU_NOTA_CH','NU_NOTA_CN'])
 
 final = (data['NU_NOTA_REDACAO']*3+data['NU_NOTA_MT']*3+data['NU_NOTA_CN']*2+data['NU_NOTA_LC']*1.5+data['NU_NOTA_CH'])/5
-
-print(final)
-#print(data[['NU_INSCRICAO', 'NU_IDADE','NU_NOTA_REDACAO','NU_NOTA_MT','NU_NOTA_LC','NU_NOTA_CH','NU_NOTA_CN']])
+data['final'] = final
+data = data.sort_values(by=['final'], ascending=False)
+#print(final)
+print("token : 1234")
+print("email : placido_antonio@estudante.sc.senai.br")
+print("answer :")
+print(data.head(20))
